e8411f047b0d7957de2aec9520642350708a5096
Mitu kr Rabha <mitukr2@gmail.com>
2024-05-29 08:19:23 +0530

    Add files via upload

---
+++ countingSort.java
@@ -0,0 +1,74 @@
+// import java.util.*;
+
+// public class countingSort {
+//     public static void CountingSort(int arr[]){
+
+//         int largest =Integer.MIN_VALUE;
+//         for(int i=0; i<arr.length; i++){
+//             largest = Math.max(largest,arr[i]);
+//         }
+
+//         int count []= new int[largest+1];
+
+//         for(int i =0; i<arr.length; i++){
+//             count[arr[i]]++;
+
+//         }
+
+//         //sorting
+
+//         int j = 0;
+//         for(int i = 0; i<count.length; i++ ){
+//             while(count[i]>0){
+//                 arr[j] = i;
+//                 j++;
+//                 count[i]--;
+
+//             }
+
+//         }
+//     }
+// public static void main(String args[]){
+    
+//     int arr[]={5,4,1,3,2};
+//     CountingSort(arr);
+// }
+// }
+
+import java.util.*;
+
+public class countingSort {
+    public static void CountingSort(int arr[]){
+        int largest =Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest,arr[i]);
+        }
+
+        int count []= new int[largest+1];
+
+        for(int i =0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+
+        //sorting
+        int j = 0;
+        for(int i = 0; i<count.length; i++ ){
+            while(count[i]>0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[]={5,4,1,3,2};
+        CountingSort(arr);
+
+        // Print the sorted array
+        System.out.println("Sorted array:");
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
